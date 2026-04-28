ca457ee6acbe97fb81492dd497248494277bbb03
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-28 17:51:06 -0300

    implementando o repositorio HourContractRepository

---
+++ src/main/java/br/com/github/renato28/composicaospring/repositories/HourContractRepository.java
@@ -0,0 +1,7 @@
+package br.com.github.renato28.composicaospring.repositories;
+
+import br.com.github.renato28.composicaospring.entities.HourContract;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HourContractRepository extends JpaRepository<HourContract, Integer> {
+}
