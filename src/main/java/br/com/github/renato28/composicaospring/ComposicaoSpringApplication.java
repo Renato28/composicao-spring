fb8b4c0f61cec9677270ebf3b25471efeb243fc4
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-28 17:19:50 -0300

    projeto criado

---
+++ src/main/java/br/com/github/renato28/composicaospring/ComposicaoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.renato28.composicaospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComposicaoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComposicaoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/github/renato28/composicaospring/ComposicaoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.renato28.composicaospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposicaoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
